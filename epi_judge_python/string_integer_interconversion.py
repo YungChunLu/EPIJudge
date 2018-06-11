f88052e582594ec95f70361b2789ad0495604f38
ArthurLu <newmilktea@gmail.com>
2018-06-11 22:58:02 +0800

    feat: Completed 6-1 / Interconvert Strings And Integers.

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -4,12 +4,28 @@ from test_framework.test_failure import TestFailure
 
 def int_to_string(x):
     # TODO - you fill in here.
-    return ''
+    if x == 0:
+        return "0"
+    is_negative, zero_code, ans = False, ord("0"), ""
+    if x < 0:
+        is_negative = True
+        x *= -1
+    while x:
+        ans = chr(x % 10 + zero_code) + ans
+        x = x // 10
+    return "-" + ans if is_negative else ans
 
 
 def string_to_int(s):
     # TODO - you fill in here.
-    return 0
+    is_negative, ans, zero_code = False, 0, ord("0")
+    if s[0] == "-":
+        is_negative = True
+        s = s[1:]
+    while s:
+        ans = ans * 10 + (ord(s[0]) - zero_code)
+        s = s[1:]
+    return ans * -1 if is_negative else ans
 
 
 def wrapper(x, s):
