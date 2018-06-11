7e7703d3ba24cc6c68810614d7771770353b5178
ArthurLu <newmilktea@gmail.com>
2018-06-11 23:12:17 +0800

    feat: Completed 7-1 / Merge Two Sorted Lists.

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def merge_two_sorted_lists(L1, L2):
     # TODO - you fill in here.
-    return None
+    merged_list = ListNode()
+    dummy_head = merged_list
+    while L1 and L2:
+        if L1.data < L2.data:
+            dummy_head.next = ListNode(data=L1.data)
+            L1 = L1.next
+        else:
+            dummy_head.next = ListNode(data=L2.data)
+            L2 = L2.next
+        dummy_head = dummy_head.next
+    if L1:
+        dummy_head.next = L1
+    elif L2:
+        dummy_head.next = L2
+    return merged_list.next
 
 
 if __name__ == '__main__':
