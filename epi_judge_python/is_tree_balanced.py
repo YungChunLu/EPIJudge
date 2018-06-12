e4cb228b61b0e89bb8377944fbb21b3bdf4a18d0
ArthurLu <newmilktea@gmail.com>
2018-06-12 09:33:06 +0800

    feat: Completed 9-1 / Test If A Binary Tree Is Height-Balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -3,7 +3,20 @@ from test_framework import generic_test
 
 def is_balanced_binary_tree(tree):
     # TODO - you fill in here.
-    return True
+    def helper(root):
+        if root:
+            is_balanced_left, h_left = helper(root.left)
+            if not is_balanced_left:
+                return is_balanced_left, 0
+            is_balanced_right, h_right = helper(root.right)
+            if not is_balanced_right:
+                return is_balanced_right, 0
+            is_balanced = abs(h_left - h_right) <= 1
+            return is_balanced, max(h_left, h_right) + 1
+        else:
+            return True, -1
+    is_balanced, _ = helper(tree)
+    return is_balanced
 
 
 if __name__ == '__main__':
