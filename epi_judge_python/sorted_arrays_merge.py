968fa7addf4e6bb75c71d87f04093083e7b7c8f8
ArthurLu <newmilktea@gmail.com>
2018-06-12 11:41:10 +0800

    feat: Completed 10-1 / Merge Sorted Files.

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
-
+import heapq
 
 def merge_sorted_arrays(sorted_arrays):
     # TODO - you fill in here.
-    return []
+    min_heap, array_len, ans = [], [], []
+    for k, array in enumerate(sorted_arrays):
+        L = len(array)
+        array_len.append(L)
+        if L:
+            heapq.heappush(min_heap, (array[0], k, 0))
+    while min_heap:
+        val, k, idx = heapq.heappop(min_heap)
+        ans.append(val)
+        idx += 1
+        if idx < array_len[k]:
+            heapq.heappush(min_heap, (sorted_arrays[k][idx], k, idx))
+    return ans
 
 
 if __name__ == '__main__':
